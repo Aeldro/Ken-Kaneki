f097b795684679dd059aa00ccc9031de950ac7cb
Alain B <almasyser@wanadoo.fr>
2023-03-15 15:20:16 +0100

    barre de navigation mode jour - 1er commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const hamburgerButton = document.querySelector(".rideau-toggler")
+const navigation = document.querySelector(".rideau")
 
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
